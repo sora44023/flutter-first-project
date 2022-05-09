8ec8450d668b6772db8139d57e48810cc3de7373
sora44023 <iwataka369167.kuma@gmail.com>
2022-05-09 16:05:48 +0900

    comment out unnecessary parts

--- lib/main.dart
+++ lib/main.dart
@@ -32,15 +32,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -49,16 +40,16 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  String _type = "偶数";
+  // String _type = "偶数";
 
   void _incrementCounter() {
     setState(() {
       _counter++;
-      if (_counter % 2 == 0) {
-        _type = "偶数";
-      } else {
-        _type = "奇数";
-      }
+      // if (_counter % 2 == 0) {
+      //   _type = "偶数";
+      // } else {
+      //   _type = "奇数";
+      // }
     });
   }
 
